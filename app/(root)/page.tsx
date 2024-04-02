4ca0ec5091eaafab6ff4518a5a1a6575434de374
aSolakov <asolakov@imm.dev>
2024-04-02 14:44:12 +0300

    Created Mobile Navigation, Clerk Authenthication & MongoDB connected

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {UserButton} from "@clerk/nextjs";
+
+const Home = () => {
+    return (
+        <div>
+            A
+        </div>
+    );
+};
+
+export default Home;
